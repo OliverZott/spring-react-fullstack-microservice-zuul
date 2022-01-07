dc7cff060f45350773562e3ac8e84f5f5a7d3c71
OliverZott <zott_oliver@web.de>
2022-01-07 11:29:52 +0100

    Add cors

--- src/main/java/com/velo/zuulgatewayservice/ZuulGatewayServiceApplication.java
+++ src/main/java/com/velo/zuulgatewayservice/ZuulGatewayServiceApplication.java
@@ -2,12 +2,29 @@ package com.velo.zuulgatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableZuulProxy  // automatically redirect with this gateway-service-project
+@EnableEurekaClient
 public class ZuulGatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayServiceApplication.class, args);
+    }
 
+    // add cors
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
